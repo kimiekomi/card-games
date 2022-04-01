27523f243981f1000567cf8d73a8746caffcfa7b
Shelby Kim <93174976+kimiekomi@users.noreply.github.com>
2022-04-01 16:59:59 +0000

    modified lets_battle to return boolean

--- war_games.py
+++ war_games.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 
-debug = True
+import random
+
+debug = False
 trace = True
 
-def deal():
-    if debug: print("called game()")
+def build_deck():
+    if debug: print("called deck()")
 
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]        
     ranks = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"]
@@ -15,7 +17,17 @@ def deal():
         for rank in ranks:
             deck.append(f"{rank} of {suit}")
 
-    if trace: print(f"deck: {deck}")
+    # if trace: print(f"deck({len(deck)}): {deck}")
+
+    random.shuffle(deck)
+
+    return deck
+
+
+def deal_cards():
+    if debug: print("called deal()")
+
+    deck = build_deck()
 
     player_deck = []
     computer_deck = []
@@ -25,18 +37,24 @@ def deal():
         player_deck.insert(0, deck.pop())
         computer_deck.insert(0, deck.pop())
 
-    if trace: print(f"\nplayer deck({len(player_deck)}): {player_deck}")
+    # if trace: print(f"\nplayer deck({len(player_deck)}): {player_deck}")
+    # if trace: print(f"\ncomputer deck({len(computer_deck)}): {computer_deck}")
+
+    return player_deck, computer_deck, cards_on_table
+
+
+def play_game():
+    if debug: print("called play_game()")
+
+    player_deck = deal_cards()[0]
+    computer_deck = deal_cards()[1]
+    cards_on_table = deal_cards()[2]
+
+    if trace: print(f"player deck({len(player_deck)}), computer deck({len(computer_deck)}), cards on table({len(cards_on_table)})")
     
-    if trace: print(f"\ncomputer deck({len(computer_deck)}): {computer_deck}")
-
-
-def play():
-    if debug: print("called play()")
-
     while len(player_deck) != 0 and len(computer_deck) != 0:
         if trace: print("\ngame_loop")
-
-        if trace: print(f"cards on table({len(cards_on_table)}): {self.cards_on_table}")
+        if trace: print(f"cards on table({len(cards_on_table)}): {cards_on_table}")
         
         player_battle_card = player_deck.pop()
         cards_on_table.append(player_battle_card)
@@ -49,60 +67,68 @@ def play():
         if player_battle_card == computer_battle_card:
             if trace: print("*** time for war ***")
                 
-            lets_war()
+            lets_war(player_deck, computer_deck, cards_on_table)
             continue
 
         if trace: print("*** time for battle ***")
 
         lets_battle(player_battle_card, computer_battle_card)
 
-        print(f"player score: {len(self.player_deck)}, computer score: {len(self.computer_deck)}")
-    
-        if trace: print(f"\nplayer deck({len(self.player_deck)}): {self.player_deck}\n\ncomputer deck({len(self.computer_deck)}): {self.computer_deck}")
+        if lets_battle:
+            for card in cards_on_table:
+                player_deck.insert(0, card)
 
+        else:
+            for card in cards_on_table:
+                computer_deck.insert(0, card)
+
+        cards_on_table = []
+    
+        if trace: print(f"table cleared: {len(cards_on_table)} cards on table")
+
+        print(f"player score: {len(player_deck)}, computer score: {len(computer_deck)}")
+    
+        if trace: print(f"\nplayer deck({len(player_deck)}): {player_deck}\n\ncomputer deck({len(computer_deck)}): {computer_deck}")
 
     # Game has ended
-    if len(self.player_deck) == 0:
+    if len(player_deck) == 0:
         print("player deck empty\n\n>>> Game Over...Computer Won War\n")
 
     else:
         print("computer deck empty\n\n>>> Game Over...Player Won War\n")
 
     
-# def lets_war(self):
+def lets_war(player_deck, computer_deck, cards_on_table):
 
-#     if debug: print("lets_war()")
+    if debug: print("lets_war()")
 
-#     for i in range(3):
+    for i in range(3):
         
-#         if len(self.player_deck) != 0 and len(self.computer_deck) != 0:
-#             self.cards_on_table.append(self.player_deck.pop())
-#             self.cards_on_table.append(self.computer_deck.pop())
+        if len(player_deck) != 0 and len(computer_deck) != 0:
+            cards_on_table.append(player_deck.pop())
+            cards_on_table.append(computer_deck.pop())
 
-#     if trace: print(f"cards on table({len(self.cards_on_table)}): {self.cards_on_table}")
+    if trace: print(f"cards on table({len(cards_on_table)}): {cards_on_table}")
 
     
-# def lets_battle(self, player_card, computer_card):
+def lets_battle(player_card, computer_card):
 
-#     if debug: print("lets_battle()")
+    if debug: print("lets_battle()")
 
-#     if player_card > computer_card:
-#         print("<player card is higher>")
+    if player_card > computer_card:
+        print("<player card is higher>")
+        return True
 
-#         for card in self.cards_on_table:
-#             self.player_deck.insert(0, card)
-
-#     else:
-#         print("<computer card is higher>")
+    print("<computer card is higher>")
+    return False
         
-#         for card in self.cards_on_table:
-#             self.computer_deck.insert(0, card)
-
-#     self.cards_on_table = []
-    
-#     if trace: print(f"table cleared: {len(self.cards_on_table)} cards on table")
-
 
 if __name__ == "__main__":
-    game()
+    # print(build_deck())
+    
+    # print(f"player deck: {deal_cards()[0]}\n")
+    # print(f"computer deck: {deal_cards()[1]}\n")
+    # print(f"cards on table: {deal_cards()[2]}")
+
+    play_game()
 
